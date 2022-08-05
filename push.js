6b2d9b3a7cf218f59f8313efb8183eedc06b7b5d
Han_in9 <dlsrn0945@gmail.com>
2022-08-06 00:21:18 +0900

    fix

--- push.js
+++ push.js
@@ -5,8 +5,21 @@ let vapidKeys={
     privateKey: '2B9MoIhq2WJ6RuT5gYmpjJ5bwJp2B359uAAA2ctYCQY'
   }
 
-push.setVapidDetails('mailto:test@cod.co.uk',vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails(
+    'mailto:test@cod.co.uk',
+    vapidKeys.publicKey, 
+    vapidKeys.privateKey
+);
 
-let sub={};
+let sub={
+    endpoint:
+    "https://fcm.googleapis.com/fcm/send/d2RoqsYnT-I:APA91bHR8WIK-RlZp64jCg0Gnt31g2XNPuCXde2XzMeqJNZebvAEngS7XK9eo4LTfrgHtu2jbWNplAgVnM4fpFbw0pf82ZeZx8b8DRAM8A4ITpCLasjBw88P4t-e_Gt_L0Yni6DH8_xY",
+    expirationTime:null,
+    keys:{
+        p256dh:
+            "BBdJBxq6mwZulVpyLk0C0LoDAvMPN7EUjDof4LjXcw60vo-0Vzh7BffEz0S607jIxnblORVCqPDL_HEZqT6mhZk",
+        auth:"tpxq6kSQNjG6OH6AqLrniw"
+    }
+};
 
 push.sendNotification(sub, 'test message')
